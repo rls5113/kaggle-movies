b142b313456408c696397fb972f79b5c9283b11e
rls5113 <stuartrl@comcast.net>
2021-05-12 13:54:49 -0500

    initial spring boot project

---
+++ src/test/java/com/kaggle/movies/KaggleMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaggle.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaggleMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
