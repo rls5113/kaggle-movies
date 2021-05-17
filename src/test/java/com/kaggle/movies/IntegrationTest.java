0203695650b4c2cdff1cb75421b9941f6d39b66c
rls5113 <stuartrl@comcast.net>
2021-05-17 09:58:12 -0500

    remove unused imports

--- src/test/java/com/kaggle/movies/IntegrationTest.java
+++ src/test/java/com/kaggle/movies/IntegrationTest.java
@@ -1,19 +1,14 @@
 package com.kaggle.movies;
 
 import com.kaggle.movies.model.Movie;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
